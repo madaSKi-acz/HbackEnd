e3d706878e057a5928326a304e3e038815bd6db8
sok <sokgithub@gmail.com>
2025-12-17 15:55:14 +0700

    first hit csrf

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
     'supports_credentials' => true,
 ];
